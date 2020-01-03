449b37ae3fcd8a46549ba9885b2b0a600e6ef9ce
Badal <nabizadebadal@gmail.com>
2020-01-03 14:04:09 -0500

    add gen_bins_with_bin_width algorithm

--- hexbin_algorithms.py
+++ hexbin_algorithms.py
@@ -164,5 +164,56 @@ def gen_bins_with_polycollection(data: list, gridsize=366):
     return sorted_hexagon_centers_gt_1, sorted_vehicle_counts_gt_1
 
 
+def _nearest_bins_centerpoint(value: float, scale: float):
+    """
+    Returns the nearest two centerpoint positions (along the same axis) for the
+    closest bins to the specified value.
+    :param value: Either the x or y value of the point
+    :param scale: The scale of the hexagons
+    :returns: tuple of closest bin centerpoints
+    """
+    div = math.floor(value/(scale / 2))
+    rounded = scale / 2 * (div + (1 if div % 2 == 1 else 0))
+    rounded_scaled = scale / 2 * (div + (1 if div % 2 == 0 else 0))
+    return [rounded, rounded_scaled]
 
+
+def _distance(x: float, y: float, x1: float, y1: float):
+    """
+    Returns the Euclidean distance from [x,y] to [x1,y1]
+    """
+    return math.sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1))
+
+
+def gen_bins_with_bin_width(data: list, bin_width: float):
+    """
+    From the given array of data points ([[x, y], ...]), generate an array of
+    hexagonal bins of given bin_width
+    :param data: list of latitude and longitude pairs.
+    :param bin_width: the size of the bins to create
+    """
+    bins = {}
+    data = np.array(data)
+    for p in data:
+        x, y = p[0], p[1]
+
+        px_nearest = _nearest_bins_centerpoint(x, bin_width)
+        py_nearest = _nearest_bins_centerpoint(y, bin_width * SQRT3)
+
+        z1 = _distance(x, y, px_nearest[0], py_nearest[0])
+        z2 = _distance(x, y, px_nearest[1], py_nearest[1])
+
+        if z1 < z2:
+            bin = str([px_nearest[0], py_nearest[0]])
+        else:
+            bin = str([px_nearest[1], py_nearest[1]])
+
+        bins[bin] = 1 + bins.get(bin, 0)
+
+    hexagon_centers = list()
+    vehicle_counts = list()
+    for k, v in bins.items():
+        hexagon_centers.append(eval(k))
+        vehicle_counts.append(v)
+    return hexagon_centers, vehicle_counts
 
