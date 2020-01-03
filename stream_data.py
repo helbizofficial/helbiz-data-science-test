0709769c2b439a437b6d0d31c49712568405fc90
Badal <nabizadebadal@gmail.com>
2020-01-03 13:10:19 -0500

    update read_data function

--- stream_data.py
+++ stream_data.py
@@ -1,4 +1,4 @@
-import requests
+import requests, logging
 
 
 def read_data(url_list: list):
@@ -10,23 +10,32 @@ def read_data(url_list: list):
     for url in url_list:
         try:
             r = requests.get(url)
-        except requests.exceptions.ConnectionError as error:
-            continue
+            # If the response code is OK, process data.
+            if r.status_code == 200:
+                json_data = r.json()
+                lats_and_lons = list()
+                data = json_data.get('data', 0)
 
-        if r.status_code == 200:
-            json_data = r.json()
-            lats_and_lons = list()
-            data = json_data.get('data', 0)
+                if not data:
+                    vehciles = json_data['bikes']
+                else:
+                    vehciles = data['bikes']
 
-            if not data:
-                vehciles = json_data['bikes']
+                for vehicle in vehciles:
+                    latitude = float(vehicle['lat'])
+                    longitude = float(vehicle['lon'])
+                    lats_and_lons.append([latitude, longitude])
+                coords.extend(lats_and_lons)
             else:
-                vehciles = data['bikes']
+                logging.info(f'response to the request is {r.status_code}')
+        # If there is a connection error to the url, continue.
+        except requests.exceptions.ConnectionError as error:
+            logging.info(f'connection error to {url}')
+            continue
+    # If data is retrieved from the urls, return it.
+    if len(coords):
+        return coords
+    # Else return None
+    return None
 
-            for vehicle in vehciles:
-                latitude = float(vehicle['lat'])
-                longitude = float(vehicle['lon'])
-                lats_and_lons.append([latitude, longitude])
-            coords.extend(lats_and_lons)
 
-    return coords
